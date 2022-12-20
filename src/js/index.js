2fdefb4235fcde944124a5f8f9c848a21d0df51d
Moshkin Denis (Frontend) <moshkin.denis.007@yandex.ru>
2022-12-20 20:44:20 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,52 @@
 function startPage() {
-    function updateOptionClass(state, label) {
-        if (state) {
-            document.getElementById("page_wrap").classList.add(`${label}-enable`);
-        }
-        else {
-            document
-                .getElementById("page_wrap")
-                .classList.remove(`${label}-enable`);
-        }
-    }
-    let checkboxLabels = [
-        "ad-block-label",
-        "recommendation-block-label",
-        "copy-post-block-label",
-    ];
-    checkboxLabels.forEach((label) => {
-        chrome.storage.local.get(label, (item) => {
-            if (!item[label] !== undefined) {
-                chrome.storage.local.set({ [label]: true });
+    if (window.location.host === "vk.com") {
+        function updateOptionClass(state, label) {
+            if (state) {
+                document
+                    .getElementById("page_wrap")
+                    .classList.add(`${label}-enable`);
             }
-            updateOptionClass(item[label], label);
-        });
-    });
-    chrome.storage.onChanged.addListener((changes, areaName) => {
-        for (let prop in changes) {
-            updateOptionClass(changes[prop].newValue, prop);
-            chrome.storage.local.set({ [prop]: changes[prop].newValue });
+            else {
+                document
+                    .getElementById("page_wrap")
+                    .classList.remove(`${label}-enable`);
+            }
         }
-    });
-    document.querySelectorAll(".wall_marked_as_ads").forEach((item) => {
-        item.closest(".feed_row").classList.add("post_marked_as_ads");
-    });
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach(function (mutation) {
-            mutation.addedNodes.forEach((item) => {
-                if (item.querySelectorAll(".wall_marked_as_ads").length) {
-                    item.classList.add("post_marked_as_ads");
+        let checkboxLabels = [
+            "ad-block-label",
+            "recommendation-block-label",
+            "copy-post-block-label",
+        ];
+        checkboxLabels.forEach((label) => {
+            chrome.storage.local.get(label, (item) => {
+                if (!item[label] !== undefined) {
+                    chrome.storage.local.set({ [label]: true });
                 }
+                updateOptionClass(item[label], label);
             });
         });
-    });
-    observer.observe(document.getElementById("feed_rows"), {
-        childList: true,
-    });
+        chrome.storage.onChanged.addListener((changes, areaName) => {
+            for (let prop in changes) {
+                updateOptionClass(changes[prop].newValue, prop);
+                chrome.storage.local.set({ [prop]: changes[prop].newValue });
+            }
+        });
+        document.querySelectorAll(".wall_marked_as_ads").forEach((item) => {
+            item.closest(".feed_row").classList.add("post_marked_as_ads");
+        });
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach(function (mutation) {
+                mutation.addedNodes.forEach((item) => {
+                    if (item.querySelectorAll(".wall_marked_as_ads").length) {
+                        item.classList.add("post_marked_as_ads");
+                    }
+                });
+            });
+        });
+        observer.observe(document.getElementById("feed_rows"), {
+            childList: true,
+        });
+    }
 }
 startPage();
 window.history.pushState = new Proxy(window.history.pushState, {
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,54 +1,58 @@
 function startPage() {
-	function updateOptionClass(state: boolean, label: string) {
-		if (state) {
-			document.getElementById("page_wrap").classList.add(`${label}-enable`);
-		} else {
-			document
-				.getElementById("page_wrap")
-				.classList.remove(`${label}-enable`);
-		}
-	}
-
-	let checkboxLabels: string[] = [
-		"ad-block-label",
-		"recommendation-block-label",
-		"copy-post-block-label",
-	];
-
-	checkboxLabels.forEach((label: string) => {
-		chrome.storage.local.get(label, (item) => {
-			if (!item[label] !== undefined) {
-				chrome.storage.local.set({ [label]: true });
+	if (window.location.host === "vk.com") {
+		function updateOptionClass(state: boolean, label: string) {
+			if (state) {
+				document
+					.getElementById("page_wrap")
+					.classList.add(`${label}-enable`);
+			} else {
+				document
+					.getElementById("page_wrap")
+					.classList.remove(`${label}-enable`);
 			}
-
-			updateOptionClass(item[label], label);
-		});
-	});
-
-	chrome.storage.onChanged.addListener((changes: any, areaName: string) => {
-		for (let prop in changes) {
-			updateOptionClass(changes[prop].newValue, prop);
-			chrome.storage.local.set({ [prop]: changes[prop].newValue });
 		}
-	});
 
-	document.querySelectorAll(".wall_marked_as_ads").forEach((item) => {
-		item.closest(".feed_row").classList.add("post_marked_as_ads");
-	});
+		let checkboxLabels: string[] = [
+			"ad-block-label",
+			"recommendation-block-label",
+			"copy-post-block-label",
+		];
 
-	const observer = new MutationObserver((mutations) => {
-		mutations.forEach(function (mutation) {
-			mutation.addedNodes.forEach((item: HTMLElement) => {
-				if (item.querySelectorAll(".wall_marked_as_ads").length) {
-					item.classList.add("post_marked_as_ads");
+		checkboxLabels.forEach((label: string) => {
+			chrome.storage.local.get(label, (item) => {
+				if (!item[label] !== undefined) {
+					chrome.storage.local.set({ [label]: true });
 				}
+
+				updateOptionClass(item[label], label);
 			});
 		});
-	});
 
-	observer.observe(document.getElementById("feed_rows"), {
-		childList: true,
-	});
+		chrome.storage.onChanged.addListener((changes: any, areaName: string) => {
+			for (let prop in changes) {
+				updateOptionClass(changes[prop].newValue, prop);
+				chrome.storage.local.set({ [prop]: changes[prop].newValue });
+			}
+		});
+
+		document.querySelectorAll(".wall_marked_as_ads").forEach((item) => {
+			item.closest(".feed_row").classList.add("post_marked_as_ads");
+		});
+
+		const observer = new MutationObserver((mutations) => {
+			mutations.forEach(function (mutation) {
+				mutation.addedNodes.forEach((item: HTMLElement) => {
+					if (item.querySelectorAll(".wall_marked_as_ads").length) {
+						item.classList.add("post_marked_as_ads");
+					}
+				});
+			});
+		});
+
+		observer.observe(document.getElementById("feed_rows"), {
+			childList: true,
+		});
+	}
 }
 
 startPage();
